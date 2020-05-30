443f13a4c76cc442c1c95ead897c03062008e8df
carlpadilla <carlpadilla@protonmail.com>
2020-05-29 22:50:48 -0400

    saving init

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,7 +2,9 @@
 
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.prev = prev
+        self.next = next
 
     def append(self, item):
         pass
