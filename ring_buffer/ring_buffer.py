b006566f1acf2b4a3bffaabda5845c1d369409bc
Carl Padilla <carlpadilla@protonmail.com>
2020-06-01 12:43:34 -0400

    complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,11 +3,18 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.prev = prev
-        self.next = next
+        self.storage = []
+        self.oldest = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage[self.oldest] = item
+            if self.oldest < len(self.storage) - 1:
+                self.oldest += 1
+            else:
+                self.oldest = 0
 
     def get(self):
-        pass
+        return self.storage
